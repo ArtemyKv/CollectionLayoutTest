970da75da1a026cec415da19cc1786969c2f602a
ArtemKvashnin <artemykv@gmail.com>
2023-07-04 09:01:12 +0500

    add new files to project

---
+++ CollectionLayoutTest/ListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ListCell.swift
+//  CollectionLayoutTest
+//
+//  Created by Artem Kvashnin on 04.07.2023.
+//
+
+import Foundation
---
+++ CollectionLayoutTest/UIColor+Random.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Random.swift
+//  CollectionLayoutTest
+//
+//  Created by Artem Kvashnin on 04.07.2023.
+//
+
+import Foundation
